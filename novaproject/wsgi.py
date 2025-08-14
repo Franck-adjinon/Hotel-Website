eec7270f239ce95f3e5d2c618712e4067d998c3f
Franck Adjinon <franckadjinon@gmail.com>
2025-08-14 09:44:18 +0000

    fix(media):Servir les médias via whitenoise (add_files) pour une démo sur render

--- novaproject/wsgi.py
+++ novaproject/wsgi.py
@@ -1,21 +1,21 @@
-"""
-WSGI config for novaproject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
 from django.core.wsgi import get_wsgi_application
-from django.conf import settings 
-# Check
-print("MEDIA_ROOT content:", os.listdir(settings.MEDIA_ROOT))
+from whitenoise import WhiteNoise
 
+# le settings module
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "novaproject.settings.prod")
 
+# 
 application = get_wsgi_application()
+
+# importer settings
+from django.conf import settings
+import os as pyos 
+
+print("MEDIA_ROOT content:", pyos.listdir(settings.MEDIA_ROOT))
+
+# WhiteNoise pour les statics
+application = WhiteNoise(application, root=str(settings.STATIC_ROOT))
 
 # Monter le dossier MEDIA sous /media/
 if hasattr(settings, "MEDIA_ROOT") and settings.MEDIA_ROOT:
